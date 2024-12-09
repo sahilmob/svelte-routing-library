e0bb4591932cbc9048e146690b2db3684a127e94
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2024-12-09 20:21:54 +0300

    extracting routes by traversing the folder structure

--- src/generated.ts
+++ src/generated.ts
@@ -1,7 +1,47 @@
+import { createRouter } from "./lib/routing";
 
-    import { createRouter } from "./lib/routing";
-    createRouter({
-        routes: [],
-        target: document.getElementById("app");
-    });
-    
+createRouter({
+  routes: [
+    {
+      url: /^\/a\/[...rest]\/?$/,
+      params: [],
+      components: [() => import("./$/a/[...rest].svelte")],
+    },
+    {
+      url: /^\/b\/?$/,
+      params: [],
+      components: [() => import("./$/b.svelte")],
+    },
+    {
+      url: /^\/c\/?$/,
+      params: [],
+      components: [() => import("./$/c.svelte")],
+    },
+    {
+      url: /^\/\/?$/,
+      params: [],
+      components: [() => import("./$/index.svelte")],
+    },
+    {
+      url: /^\/item\/[shopId]\/[itemId]\/?$/,
+      params: [],
+      components: [() => import("./$/item/[shopId]/[itemId].svelte")],
+    },
+    {
+      url: /^\/item\/__layout\/?$/,
+      params: [],
+      components: [() => import("./$/item/__layout.svelte")],
+    },
+    {
+      url: /^\/shop\/[shopId]\/?$/,
+      params: [],
+      components: [() => import("./$/shop/[shopId].svelte")],
+    },
+    {
+      url: /^\/shop\/__layout\/?$/,
+      params: [],
+      components: [() => import("./$/shop/__layout.svelte")],
+    },
+  ],
+  target: document.getElementById("app")!,
+});
--- src/lib/routing/scripts/generate-routes.ts
+++ src/lib/routing/scripts/generate-routes.ts
@@ -1,20 +1,69 @@
 import * as fs from "fs";
 import * as path from "path";
 
+type Route = {
+  layouts: [];
+  relativePath: string;
+  componentPath: string;
+};
+
 const cwd = process.cwd();
 const outputFilePath = path.join(cwd, "src/generated.ts");
 const inputFolder = path.join(cwd, "src/$");
 
-const routes = fs.readdirSync(inputFolder);
+function exploreFolders(rootRoutesDir: string) {
+  const routes: Array<Route> = [];
+  function _explore(folderPath: string) {
+    const files = fs.readdirSync(folderPath);
+    for (const file of files) {
+      const filePath = path.join(folderPath, file);
+      const isDirectory = fs.statSync(filePath).isDirectory();
+      if (isDirectory) {
+        _explore(filePath);
+      } else {
+        routes.push({
+          componentPath: filePath,
+          relativePath: path.relative(rootRoutesDir, filePath),
+          layouts: [],
+        });
+      }
+    }
+  }
+
+  _explore(rootRoutesDir);
+
+  return routes;
+}
+
+const routes = exploreFolders(inputFolder);
 console.log(routes);
 
 fs.writeFileSync(
   outputFilePath,
   `
     import { createRouter } from "./lib/routing";
+
     createRouter({
-        routes: [],
-        target: document.getElementById("app");
+        routes: [
+            ${routes
+              .map(({ relativePath, layouts }) => {
+                const component = relativePath.replace(/\.svelte$/, "");
+                const segments = component.split("/");
+                if (segments[segments.length - 1] === "index") {
+                  segments[segments.length - 1] = "";
+                }
+                segments.join("\\/");
+                const regExp = `/^\\/${segments.join("\\/")}\\/?$/`;
+                return `{
+                url: ${regExp},
+                params: [],
+                components:[() => import('./$/${relativePath}')]
+                }
+              `;
+              })
+              .join(",\n")}
+        ],
+        target: document.getElementById("app")!
     });
     `,
   "utf-8"
