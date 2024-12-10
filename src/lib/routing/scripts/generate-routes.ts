903d89b79251e7116e3c290b6373d0a49fa16f5e
Sahil H. Mubaideen <Sahil.mubaideen@network.global>
2024-12-10 11:21:16 +0300

    extracting parameters from file paths into regular expressions

--- src/lib/routing/scripts/generate-routes.ts
+++ src/lib/routing/scripts/generate-routes.ts
@@ -1,70 +1,158 @@
 import * as fs from "fs";
 import * as path from "path";
 
-type Route = {
-  layouts: [];
-  relativePath: string;
-  componentPath: string;
-};
+// TODO:
+// - [ ] generates route definition
+// - [ ] handle layout
 
 const cwd = process.cwd();
+
 const outputFilePath = path.join(cwd, "src/generated.ts");
 const inputFolder = path.join(cwd, "src/$");
 
-function exploreFolders(rootRoutesDir: string) {
-  const routes: Array<Route> = [];
-  function _explore(folderPath: string) {
+const routes = exploreFolders(inputFolder);
+
+fs.writeFileSync(
+  outputFilePath,
+  `
+import { createRouting } from './lib/routing';
+createRouting({
+  routes: [
+    ${routes
+      .map(({ componentPath, relativePath, layouts }) => {
+        const { regex, params } = getRegExpAndParams(relativePath);
+        return `{
+        url: ${regex},
+        params: [
+          ${params
+            .map(
+              ({ name, rest }) =>
+                `{ name: ${JSON.stringify(name)}, rest: ${
+                  rest ? "true" : "false"
+                } }`
+            )
+            .join(",\n")}
+        ],
+        components: [
+          () => import('./$/${relativePath}')
+        ]
+      }`;
+      })
+      .join(",\n")}
+  ],
+  target: document.getElementById('app'),
+});
+`,
+  "utf8"
+);
+
+function getRegExpAndParams(relativePath) {
+  const component = relativePath.replace(/\.svelte$/, "");
+  const segments = component.split("/");
+  const length = segments.length;
+
+  const params = [];
+  const regexSegments = [];
+
+  for (let i = 0; i < length; i++) {
+    let segment = segments[i];
+    if (i === length - 1 && segment === "index") {
+      segment = "";
+    }
+    if (segment.indexOf("[") > -1) {
+      let isOpening = false;
+      let paramName = "";
+      let regexStr = "";
+      let rest = false;
+      // sanity checking
+      // a[baaac
+      //       ^
+      // partname: 'a' + '([^/]+)'
+      for (let index = 0; index < segment.length; index++) {
+        const char = segment[index];
+        if (char === "[") {
+          if (isOpening) {
+            throw new Error(
+              `Invalid path ${relativePath}, encounter "[" after another "["`
+            );
+          }
+          isOpening = true;
+          if (
+            segment[index + 1] === "." &&
+            segment[index + 2] === "." &&
+            segment[index + 3] === "."
+          ) {
+            rest = true;
+            index += 3;
+          } else {
+            rest = false;
+          }
+          paramName = "";
+        } else if (char === "]") {
+          if (!isOpening) {
+            throw new Error(
+              `Invalid path ${relativePath}, encounter "]" before any "["`
+            );
+          }
+          if (paramName === "") {
+            throw new Error(
+              `Invalid path ${relativePath}, encounter "[]" without parameter name`
+            );
+          }
+          params.push({
+            name: paramName,
+            rest,
+          });
+          if (rest) {
+            regexStr += "(?:|\\/(.+))";
+          } else {
+            regexStr += "([^/]+)";
+          }
+          isOpening = false;
+        } else {
+          if (isOpening) {
+            paramName += char;
+          } else {
+            regexStr += char;
+          }
+        }
+      }
+
+      if (isOpening) {
+        throw new Error(`Invalid path ${relativePath}, unclosed "["`);
+      }
+
+      segment = regexStr;
+    }
+
+    regexSegments.push(segment);
+  }
+
+  return {
+    regex: `/^\\/${regexSegments.join("\\/")}\\/?$/`,
+    params,
+  };
+}
+
+function exploreFolders(rootRouteDirectory) {
+  function _explore(folderPath) {
     const files = fs.readdirSync(folderPath);
     for (const file of files) {
       const filePath = path.join(folderPath, file);
       const isDirectory = fs.statSync(filePath).isDirectory();
+
       if (isDirectory) {
         _explore(filePath);
       } else {
         routes.push({
           componentPath: filePath,
-          relativePath: path.relative(rootRoutesDir, filePath),
+          relativePath: path.relative(rootRouteDirectory, filePath),
           layouts: [],
         });
       }
     }
   }
-
-  _explore(rootRoutesDir);
-
+  const routes = [];
+  _explore(rootRouteDirectory);
   return routes;
 }
-
-const routes = exploreFolders(inputFolder);
-console.log(routes);
-
-fs.writeFileSync(
-  outputFilePath,
-  `
-    import { createRouter } from "./lib/routing";
-
-    createRouter({
-        routes: [
-            ${routes
-              .map(({ relativePath, layouts }) => {
-                const component = relativePath.replace(/\.svelte$/, "");
-                const segments = component.split("/");
-                if (segments[segments.length - 1] === "index") {
-                  segments[segments.length - 1] = "";
-                }
-                segments.join("\\/");
-                const regExp = `/^\\/${segments.join("\\/")}\\/?$/`;
-                return `{
-                url: ${regExp},
-                params: [],
-                components:[() => import('./$/${relativePath}')]
-                }
-              `;
-              })
-              .join(",\n")}
-        ],
-        target: document.getElementById("app")!
-    });
-    `,
-  "utf-8"
-);
